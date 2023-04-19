7dc3dcf1a236038fa49215b27ea4453f6a91d943
Vadym Chutur <falloutgreck@gmail.com>
2023-04-19 20:58:09 +0300

    update module 3

---
+++ js/module-1.js
@@ -0,0 +1,38 @@
+const playList = {
+  name: 'My super playlist',
+  raiting: 5,
+  tracks: ['track-1', 'track-2', 'track-3'],
+  trackCount: 3,
+};
+
+// console.log(playList.raiting);
+
+const userName = 'Mango';
+const email = 'mango@mail.com';
+
+const signUpData = {
+  userName,
+  email,
+};
+
+// console.log(signUpData);
+
+const inputName = 'color';
+const inputValue = 'tomato';
+
+const colorPickerData = {
+  [inputName]: inputValue,
+};
+
+// console.log(colorPickerData);
+
+const a = { x: 1, y: 2 };
+const b = a;
+
+// console.log(b === a);
+// console.log(b);
+
+// a.c = 100;
+
+// console.log(b);
+// console.log(a);
